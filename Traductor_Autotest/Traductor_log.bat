a3eef935a3c32d7654374f95e2003ee3da4adcc1
Abregu081 <Tomas.Abregu@mirgor.com>
2025-01-13 20:03:47 -0300

    Se suman las carpetas todos los archivos para ser montados

---
+++ Traductor_Autotest/Traductor_log.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Cambia "programa.exe" por el nombre de tu ejecutable
+call Traductor_Autotest.exe
+
+:: Pausa para que la ventana no se cierre automáticamente
+@timeout -t 40
---
+++ Traductor_Minspeccion1.1/Traductor_log.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Cambia "programa.exe" por el nombre de tu ejecutable
+call Traductor_Lo_Manual_Inspeccion_V2.exe
+
+:: Pausa para que la ventana no se cierre automáticamente
+@timeout -t 5
